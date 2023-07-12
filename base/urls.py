649f65dc9ff44c65831e628ce3bcdc016fcbd857
Rajiv Kamat <rajiv.kamat@crystalsolution.com.np>
2023-07-12 12:48:13 +0545

    Initila commit

---
+++ base/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('login/',views.loginPage,name='login'),
+    path('logout/',views.logoutUser,name='logout'),
+    path('register/',views.registerUser,name='register'),
+
+    path('',views.home, name="home"),
+    path('room/<str:pk>/', views.room, name="room"),
+    path('profile/<str:pk>/', views.userProfile, name="user-profile"),
+    path('create-room/',views.createRoom, name="create-room"),
+    path('update-room/<str:pk>/',views.updateRoom, name="update-room"),
+    path('delete-room/<str:pk>/',views.deleteRoom, name="delete-room"),
+    path('delete-message/<str:pk>/',views.deleteMessage, name="delete-message"),
+    path('update-user/',views.updateUser, name="update-user"),
+    path('topics/',views.topicsPage, name="topics"),
+    path('activity/',views.activityPage, name="activities"),
+
+    path('chat-room/<str:room>/',views.chatRoom, name="chatroom"),
+    path('user_active/<str:user_id>/', views.check_remote_user_active, name='active-user'),
+
+]
